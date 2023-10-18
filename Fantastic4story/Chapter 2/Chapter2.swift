30be5c0356fb63788bda3331d09198a689c8f96c
Kern Redd <reddkern@gmail.com>
2023-10-18 11:24:13 -0400

    went back a commit

--- Fantastic4story/Chapter 2/Chapter2.swift
+++ Fantastic4story/Chapter 2/Chapter2.swift
@@ -6,6 +6,5 @@
 import Foundation
 
 func chapterTwo() {
-        print("David and Ethan finally arrived at the stadium and Ethan could barely contain his excitement as the where entering the game")
-        
+    // Your portion of the story goes here
 }
