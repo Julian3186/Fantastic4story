039cb8a8f898a782aac862d40236b801faa3422b
julian <mrpurnell86@yahoo.com>
2023-10-18 11:45:00 -0400

    add print statement

--- Fantastic4story/Chapter 1/Chapter1.swift
+++ Fantastic4story/Chapter 1/Chapter1.swift
@@ -4,7 +4,66 @@
 //
 
 import Foundation
-
 func chapterOne() {
-    // Your portion of the story goes here
+    
+    let fathersName: String = "Kevin"
+    let sonsName: String = "David"
+    let mothersName: String = "Marsha"
+    func part0ne() {
+        print("As \(fathersName) wakes up he looks at the clock notices its 9:40 in the morning.")
+        print("As he stretches in the bed he can smell bacon cooking in the kitchen.")
+        print("He thinks to himself it must be my wife \(mothersName) cooking breakfast.")
+        print("\(fathersName) walks to the bathroom to freshen up, and he starts to think of memories of his own Father.")
+        print("\(fathersName) and his Father were not close, so he was trying to remember what he wanted to do as kid but never got the chance to.")
+        print("\(fathersName) grew up in Philadelphia and his father raised him by himself because his mother passed away when he was very young")
+    }
+    func partTwo() {
+        print("As \(fathersName) wakes up he looks at the clock notices its 9:40 in the morning.")
+        print("As he stretches in th bed he can smell bacon cooking in the kitchen.")
+        print("He thinks to himself it must be my wife \(mothersName) cooking breakfast.")
+        print("\(fathersName) walks to the bathroom to freshen up, and he starts to think of memories of his own Father.")
+        print("\(fathersName) and his Father were not close, so he was trying to remember what he wanted to do as kid but never got the chance to.")
+        print("\(fathersName) grew up in Philadelphia and his father raised him by himself because his mother passed away when he was very young")
+        print("\"I want to give my son a day I was never able to get from my Father!\"")
+        print("He says after he finished brushing his teeth while looking in the mirror")
+        print("As he walks out the bathroom he hears his wife yelling ")
+        print("\"Kevin and David come eat!\"")
+    }
+    func partThree() {
+        print("Before there son \(sonsName) could make it into the kitchen \(mothersName) pulls \(fathersName) to the side")
+        print("\"Guess what my co-worker gave me some tickets to the Eagles vs Redskins why don't you take David\"")
+        print("\(fathersName) replied")
+        print("\"that's perfect I was just planning on spending the day with him\"")
+        print("Soon after \(sonsName) walks in")
+        print("\" I'm hungry as a hostage thanks Ma\"")
+        print("\(fathersName) smirks and says")
+        print("\"Boy you always hungry after you eat get yourself together we going to the football game\"")
+        print("\(sonsName) barely swallowing his food yells", terminator: " ")
+        print("\"Bet\"")
+        print("As \(fathersName) and they leave the house...")
+    }
+    part0ne()
+    partTwo()
+    partThree()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
