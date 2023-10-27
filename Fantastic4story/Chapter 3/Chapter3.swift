9ba93122ca724129e3245fa1d7914ca496b5f14d
Marsi <marsett>
2023-10-27 10:11:55 -0400

    adding chapter#3

--- Fantastic4story/Chapter 3/Chapter3.swift
+++ Fantastic4story/Chapter 3/Chapter3.swift
@@ -3,11 +3,43 @@
 //  CLIStoryTemplate
 //
 
+//fathersName = "Kevin"
+//sonsName = "David"
+//mothersName = "Marsha"
+
+//event = "football game"
+//weather = "Sunny"
+
+
 import Foundation
 
 func chapterThree() {
     // Your portion of the story goes here
-    print("Hello Fantastic Four")
-    print("Chapter #Three")
-    print("The Game was the greatest game of the century")
+  let fathersName = "Kevin"
+  let mothersName = "Marsha"
+  let sonsName = "David"
+  let event = "football game"
+  let weather = "Sunny"
+  let ageAsDouble = 10.00
+  let Int = 10
+    
+    
+    print("\"It was such a GREAT Game!\"")
+    print("\(fathersName) and \(mothersName) could not have picked, a better first game for their son, \(sonsName) to experience.")
+    print("All the fans were so excited. The fans we’re cheering, singing, dancing for the winning team. Someone shouted,")
+    print( "\"Another great win!\"")
+    print("\(fathersName) saw the homeless man down the street from the stadium while they were  walking from the game.   He thought ")
+    print( "\"We don’t have to want for any thing, We are so Blessed This is a teachable moment or my son\"")
+    print("As they were getting closer \(fathersName) begin talking to \(sonsName) more and more about the misfortune of others and how its good to be nice to others.  \(fathersName) ask \(sonsName) would he like to share his food he never touched with the homeless man.")
+    print("\(sonsName) look at his bag and looked at his father then looked at the homeless man.  As they approach the man \(sonsName) looked at his bag again and spoke to the man asking if he would like something to eat.")
+    print( "\"Hello Sir, would like something to eat?\"")
+    print("The man’s eyes perked up as he looked at \(sonsName) and \(fathersName) then said ")
+    print( "\"yes please!\"")
+    print("\(sonsName) happily gave to the man, his bag of food and $\(Int) too.")
+    print("The man was very grateful.  He told both \(fathersName) and \(sonsName), repeatedly ")
+    print( "\"thank you thank you .\"")
+    print("Once they reached the car \(fathersName) and \(sonsName) did their SIGNATURE HAND SHAKE and smiled and one another.")
+    print("On the way home in the car \(fathersName) and \(sonsName) had the biggest smiles on their faces.  \(fathersName) was so full from the day experience. Thier team won the game, \(sonsName)'s favorite player made he winning touchdown, dont forget with a one handed catch and my son is a compassionate human being" )
+    print(" Kevin didn’t notice his son was smiling too. Just as they pulled into the driveway David said to his Dad")
+    print( "\"This has been an awesome day!\"")
 }
